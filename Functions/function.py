99cff2ba93019d83fe6c432b45c6e510189bdaf6
jonathancastillo185 <jonathancastillo185@email.com>
2024-05-12 23:20:42 -0300

    Continua la creacion del Script

--- Functions/function.py
+++ Functions/function.py
@@ -10,6 +10,32 @@ import shutil
 from bs4 import BeautifulSoup
 import requests
 
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# -------------- PARAMETROS
+
+espera = 10
+
+# -------------- 
+
+
+def inicio_driver():
+    opts = Options()
+    opts.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36")
+    
+    # En caso de querer ver el proceso con la ventana del navegador comentar la siguiente linea
+    opts.add_argument("--headless")
+    
+    driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=opts)
+
+    return driver
+    
+    
 def print_process(texto):
     terminal_width, _ = shutil.get_terminal_size()  # Obtener el ancho del terminal
     longitud_total = terminal_width - 2  # Longitud total de la línea (restando el espacio para los bordes)
@@ -23,23 +49,27 @@ def categories():
     """
         Con esta funcion extraemos todas las categorias de la pagina 'https://filmzie.com/home'
     """
+    comienzo = time.time()
     try:
-        driver = webdriver.Chrome()
+        driver = inicio_driver()
         
         driver.get("https://filmzie.com/home")
 
-        button = WebDriverWait(driver, 10).until(
+        button = WebDriverWait(driver, espera).until(
             EC.presence_of_element_located((By.CLASS_NAME, "category-burger"))
         )
 
         button.click()
 
-        menu_items = WebDriverWait(driver, 10).until(
+        menu_items = WebDriverWait(driver, espera).until(
             EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".categories .category")))
 
         categorias = [x.text.lower().replace(" ","_").replace("-","_") for x in menu_items]
 
+        fin = time.time()
+        
         print_process("Categorias capturadas correctamente.")
+        print_process(f"Categorias demoro {round((fin-comienzo),2)} segundos")
         
         return categorias[1:]
     
@@ -56,11 +86,11 @@ def get_titles(driver):
     """
     Función auxiliar para obtener los títulos de una página web.
     """
-    titulos_element = driver.find_elements(By.CSS_SELECTOR, "p.title")
+    links_element = driver.find_elements(By.CSS_SELECTOR, "a")
 
-    titulos = [titulo.text.lower().replace(" ", "-") for titulo in titulos_element]
+    link = [link.get_attribute("href") for link in links_element]
     
-    return titulos
+    return link
 
 
 def movies_for_category(category):
@@ -68,17 +98,16 @@ def movies_for_category(category):
     Ingresa una categoría y esta función dirigirá a la URL específica de esta categoría 
     y devolverá la cantidad de títulos dentro de la misma.
     """
+    comienzo = time.time()
     intentos = 5
+    
     for intento in range(intentos):
         try:
-            # Inicializar el navegador Chrome
-            driver = webdriver.Chrome()
+            driver = inicio_driver()
             
-            # Abrir la página web
             driver.get(f"https://filmzie.com/category/{category}")
 
-            # Esperar hasta que aparezca al menos un título
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, espera).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "p.title"))
             )
 
@@ -95,18 +124,19 @@ def movies_for_category(category):
 
                 # Calcular la nueva altura de la página
                 new_height = driver.execute_script("return document.body.scrollHeight")
-
+                
                 # Si la altura de la página ya no aumenta, salir del bucle
                 if new_height == last_height:
                     break
-
-                # Actualizar la altura de la página
+                
                 last_height = new_height
 
             # Obtener los títulos utilizando la función auxiliar
             titulos = get_titles(driver)
             
-            print_process(f"Todas las peliculas de la categoria '{category}' capturadas correctamente.")
+            fin = time.time()
+            
+            print_process(f"Todas las peliculas de la categoria '{category}' capturadas correctamente, demoro {round((fin-comienzo),2)} segundos")
             
             return titulos
 
@@ -124,35 +154,43 @@ def movies_for_category(category):
             driver.quit()
             
             
-def movies(titulo):
-    # URL de la página web a analizar
-    url = f"https://filmzie.com/content/{titulo}"
+def movies(url):
+    try:
+        # Realizar la solicitud GET a la página web
+        response = requests.get(url)
 
-    # Realizar la solicitud GET a la página web
-    response = requests.get(url)
+        # Verificar si la solicitud fue exitosa (código de estado 200)
+        if response.status_code == 200:
+            # Obtener el contenido HTML de la página
+            html_content = response.content
 
-    # Verificar si la solicitud fue exitosa (código de estado 200)
-    if response.status_code == 200:
-        # Obtener el contenido HTML de la página
-        html_content = response.content
+            # Crear un objeto BeautifulSoup
+            soup = BeautifulSoup(html_content, "html.parser")
+            
+            diccionario_extraccion = {}
+            
+            try:
+                diccionario_extraccion["titulo"] = soup.find(class_="fw-bold title").text
+                diccionario_extraccion["ano"] = soup.find(class_="year").text
+                diccionario_extraccion["duracion"] = soup.find(class_="duration").text
+                diccionario_extraccion["categorias"] = soup.find(class_="category").text
+                diccionario_extraccion["descripcion"] = soup.find(class_="fs-18").text
+                diccionario_extraccion["tipo"] = "Pelicula"
+                diccionario_extraccion["link"] = url
 
-        # Crear un objeto BeautifulSoup
-        soup = BeautifulSoup(html_content, "html.parser")
+                
+            except:
+                diccionario_extraccion["titulo"] = soup.find(class_="fw-bold title").text
+                diccionario_extraccion["ano"] = soup.find(class_="year").text
+                diccionario_extraccion["duracion"] = "0 min"
+                diccionario_extraccion["categorias"] = soup.find(class_="category").text
+                diccionario_extraccion["descripcion"] = soup.find(class_="fs-18").text
+                diccionario_extraccion["tipo"] = "Serie"
+                diccionario_extraccion["link"] = url
+                
+            return diccionario_extraccion
         
-        nombre_extraccion = ["titulo","ano","duracion","categorias","descripcion"]
-        lista_extraccion = ["fw-bold title","year","duration","category","fs-18"]
-        
-        diccionario_extraccion = {}
-        
-        for nombre , clase in zip(nombre_extraccion,lista_extraccion):
-            diccionario_extraccion[nombre] = soup.find(class_=clase).get_text()
-
-        diccionario_extraccion["link"] = url
-
-        print(diccionario_extraccion)
-        # Ahora puedes usar el objeto soup para buscar y analizar el contenido HTML según sea necesario
-        # Por ejemplo:
-        # title = soup.title
-        # print(title)
-    else:
-        print("Error al cargar la página:", response.status_code)
+        else:
+            print("Error al cargar la página:", response.status_code)
+    except Exception as e:
+        print("Se produjo un error durante la solicitud o el análisis de la página:", e)
